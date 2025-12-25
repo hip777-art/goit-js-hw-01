dcbe8534cab5e93bffd26f396e6d412d4852e86d
hip777-art <arkhip24032000@gmail.com>
2025-12-25 14:08:35 +0200

    Refactor getShippingMessage for simplicity
    Simplified the getShippingMessage function by removing unnecessary checks and using direct addition for the total price.

--- js/task-2.js
+++ js/task-2.js
@@ -1,12 +1,6 @@
-// `use strict`
-// Task 2. Product Delivery
+'use strict';
 function getShippingMessage(country, price, deliveryFee) {
-  const totalPrice = Number.parseFloat(price) + Number.parseFloat(deliveryFee);
-  if (totalPrice) {
-    return `Shipping to ${String(country)} will cost ${totalPrice} credits`;
-  } else {
-    return `Please enter valid arguments - numeric values' for the getShippingMessage function`;
-  }
+  return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
 }
 console.log(getShippingMessage('Australia', 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage('Germany', 80, 20)); // "Shipping to Germany will cost 100 credits"
