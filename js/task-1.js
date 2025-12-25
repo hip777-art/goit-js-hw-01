efd82dd6f9d96cfa750f0e93742b530bf6cb840d
hip777-art <arkhip24032000@gmail.com>
2025-12-25 14:08:05 +0200

    Simplify makeTransaction function
    Refactor makeTransaction function to simplify total price calculation and improve argument validation.

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,10 @@
-// `use strict`
-// Task 1. Ordering droids
+'use strict';
+
 function makeTransaction(quantity, pricePerDroid) {
-    const totalPrice = Number(quantity) * Number(pricePerDroid);
-    if (totalPrice) {
-        
-        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-    } else {
-        return `Please enter valid arguments - numeric values' for the makeTransaction function` ;
-    }
+  return `You ordered ${quantity} droids worth ${
+    quantity * pricePerDroid
+  } credits!`;
 }
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!" 
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
